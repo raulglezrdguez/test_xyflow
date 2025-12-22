993b0f7cd82fe40eb715d1b7058c8c53bd33c46e
Raul Glez Rdguez <raulglezrdguez69@gmail.com>
2025-12-22 17:17:29 -0500

    get diagrams removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,4 @@
 import Editor from "@/components/Editor";
-import { headers } from "next/headers";
-
-const headersList = await headers();
-const protocol = headersList.get("x-forwarded-proto") || "http";
-const host = headersList.get("host");
-const url = `${protocol}://${host}/api/diagrams`;
-
-const response = await fetch(url, {
-  method: "GET",
-});
-
-const diagrams = await response.json();
-
-console.log(diagrams);
 
 export default function Home() {
   return (
